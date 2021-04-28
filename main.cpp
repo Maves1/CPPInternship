a43a21f1fb1b89d0d76da9abfd8467f183db591d
Maves <seva.mikulik@gmail.com>
2021-04-28 19:33:56 +0300

    It's now possible to change the user and the user group! Wow!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,27 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <pwd.h>
+#include <grp.h>
+
+bool changeUser(char *userName, char *groupName) {
+    struct passwd *p = getpwnam(userName);
+    struct group *group = getgrnam(groupName);
+
+    if (p != NULL && group != NULL) {
+        if (setgid(group->gr_gid) == 0 && setuid(p->pw_uid) == 0) {
+            printf("Everything is cool!\n");
+            return true;
+        } else {
+            printf("It seems that you don't have the rights to run this program! Please run it as root\n");
+        }
+    } else if (p == NULL) {
+        printf("Something is wrong with the username\n");
+    } else {
+        printf("Something is wrong with the groupname\n");
+    }
+
+    return false;
+}
 
 void printdir(char *dir, int depth) {
     DIR *dp;
@@ -43,12 +64,7 @@ int main(int argc, char* argv[]) {
     groupName = argv[2];
     topdir = argv[3];
 
-    struct passwd *p = getpwnam(userName);
-    if (p == NULL) {
-        printf("Hmm\n");
-    } else {
-        printf("%d\n", (int) p->pw_uid);
-    }
+    changeUser(userName, groupName);
 
     printf("Directory scan of %s\n", topdir);
     printdir(topdir, 0);
